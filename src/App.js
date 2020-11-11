b3c00f56369c22ecf8a5a171fb6f930350198906
Aleksandar Jocovic <jocovic099@gmail.com>
2020-11-11 18:45:59 +0100

    Imlplemented firebase utils, inluded ability to auth user and save  user in cloudstore.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SingInAndSingUp from './pages/sing-in-and-sing-up/SingInAndSingUp';
 
 import Header from './components/header/Header';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -24,10 +24,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }) 
+          console.log(this.state)
+        })
+      }
+      else this.setState({currentUser: userAuth});
+      
     })
   }
 
--- src/pages/sing-in-and-sing-up/SingInAndSingUp.js
+++ src/pages/sing-in-and-sing-up/SingInAndSingUp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SingIn from '../../components/sing-in/SingIn'
+import SingUp from '../../components/sing-up/SingUp'
 
-/* import './sing-in-and-sing-up.styles.scss'; */
+
+import './sing-in-and-sing-up.styles.scss';
 
 const SingInAndSingUp = () => (
   <div className="sing-in-and-sing-up">
     <SingIn />
+    <SingUp />
   </div>
 
 )
