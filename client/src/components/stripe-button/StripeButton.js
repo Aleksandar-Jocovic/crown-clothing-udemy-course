a223800241a32b77280d932a8c93eeca4581a296
Aleksandar Jocovic <jocovic099@gmail.com>
2020-12-26 22:02:13 +0100

    Express code and server added.

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51HwKI6BD5oRAGqa5KG3HmjjyAmy4QujGSvD2W4EkIQXHXTEL4EAsKJNybMzQMEoKRaygStrXJ9LPRjMqAm04KNDh00wJntdttx';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful');
+    }).catch(error => {
+      console.log('payment error', error);
+      alert('There was an issue with your payment. Please make sure you are using provided credit card.');
+    })
   }
 
 
